06d7fea7fd2f260059d701aafc6649b3455f5bf6
lovro <lovro.zagar5@gmail.com>
2022-08-11 21:13:19 +0200

    Add base grid and new grid color change functionality

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 "use strict"
+
+const BASE_GRID = 16;
+
+const container = document.querySelector('#container');
+addGrid(BASE_GRID);
+
+changeColor();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let newGridNumber = +prompt("Enter new grid size");
+    container.replaceChildren();
+    addGrid(newGridNumber);
+    changeColor();
+});
+
+function addGrid(gridSize) {
+    let minWidth = 100 / gridSize;
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid-item');
+        div.style.minWidth = `${minWidth}%`;
+        container.appendChild(div);
+    }
+}
+
+function changeColor() {
+    let items = document.querySelectorAll('.grid-item');
+    items.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = 'blue';
+        });
+    });
+}
